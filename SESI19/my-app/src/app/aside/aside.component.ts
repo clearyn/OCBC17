c95f0a7f4202d3aba9b51bce7642d2ad2d4e0887
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-10-26 17:27:25 +0700

    Update two way binding

--- SESI19/my-app/src/app/aside/aside.component.ts
+++ SESI19/my-app/src/app/aside/aside.component.ts
@@ -7,7 +7,5 @@ import { Component } from "@angular/core";
 })
 
 export class AsideComponent{
-    currentCustomer = 'Maria';
-
-    title = 'Feature product:'
+    currentCustomer = 'Robert';
 }
--- SESI19/my-app/src/app/button/button.component.ts
+++ SESI19/my-app/src/app/button/button.component.ts
@@ -9,19 +9,30 @@ export class ButtonComponent {
   @Input()  currentNumber!: number;
   @Output() currentNumberChange = new EventEmitter<number>();
 
+  @Input()  items!: string[];
+  @Output() itemsChange = new EventEmitter<string[]>();
+
   onChange(event?: number){
     switch (event) {
       case 1:
-        this.currentNumber++; 
+        this.currentNumber++;
         break;
       case 2:
-        this.currentNumber = this.currentNumber - 1; 
+        this.currentNumber = this.currentNumber - 1;
         break;
       default:
         this.currentNumber = 0; 
         break;
     }
+    
+    this.addNewItem(this.currentNumber);
     this.currentNumberChange.emit(this.currentNumber);
+  }
+
+  addNewItem(value: number){
+    console.log(this.items);
+    this.items.push(value.toString());
+    this.itemsChange.emit(this.items);
   }
 
 }
