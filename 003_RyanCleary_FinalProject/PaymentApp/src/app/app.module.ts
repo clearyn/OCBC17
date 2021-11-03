57d32bf5997df1f7f67f98ab8201e9a287b07bdb
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-11-03 11:20:18 +0700

    Repair conflict

--- 003_RyanCleary_FinalProject/PaymentApp/src/app/app.module.ts
+++ 003_RyanCleary_FinalProject/PaymentApp/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
@@ -12,6 +16,9 @@ import { MaterialModule } from './material/material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     MaterialModule
   ],
   providers: [],
